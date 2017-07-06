ff599b926671dce59748c3bcb304335fb46d2221
Thomas McGuirk <thomas.mcguirk@arcadiagroup.co.uk>
2017-07-06 12:08:35 +0100

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
-var app = express();
+var multer = require('multer')
 
-app.get('/', function(req, res){
-  res.send('hello world');
+var app = express();
+var upload = multer({ dest: 'uploads/' });
+
+app.get('/', (req, res) => {
+  res.send('this is food-notifier');
+});
+
+app.post('/yum', upload.single('food'), (req, res, next) => {
+  console.log(req.file);
+  res.status(200).send();
 });
 
 app.listen(3000);
+
+// https://hooks.slack.com/services/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
