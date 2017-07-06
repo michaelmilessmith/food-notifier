6b6baa767f5825e7091bf19550b274dc4cc2db3d
Thomas McGuirk <thomas.mcguirk@arcadiagroup.co.uk>
2017-07-06 14:48:23 +0100

    uploading to imgur

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 var express = require('express');
 var multer = require('multer')
+var request = require('request-promise');
 
 var app = express();
-var upload = multer({ dest: 'uploads/' });
+var upload = multer();
 
 app.get('/', (req, res) => {
   res.send('this is food-notifier');
 });
 
 app.post('/yum', upload.single('food'), (req, res, next) => {
-  console.log(req.file);
-  res.status(200).send();
+  const options = {
+    url: 'https://api.imgur.com/3/image',
+    formData: {
+      image: req.file.buffer
+    },
+    headers: {
+      Authorization: 'Client-ID cc1a44534ad0b4e'
+    },
+    json: true
+  };
+
+  // webhook url
+  // https://hooks.slack.com/services/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
+
+  request.post(options)
+    .then((response) => {
+      const link = response.data.link;
+      res.status(200).send();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send();
+    });
 });
 
 app.listen(3000);
-
-// https://hooks.slack.com/services/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
